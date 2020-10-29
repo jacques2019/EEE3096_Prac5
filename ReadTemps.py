318a0135f8582aa7fca4f4d86dc200e5f7d73ed2
jacques2019 <jacques2019@gmail.com>
2020-10-29 13:58:54 +0200

    Temp test1

--- ReadTemps.py
+++ ReadTemps.py
@@ -4,6 +4,9 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
+mcp_0temp = 0.4
+mcp_coefficient = 10
+
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -15,5 +18,9 @@ mcp = MCP.MCP3008(spi, cs)
 
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
-print('Raw ADC Value: ', chan.value)
-print('ADC Voltage: ' + str(chan.voltage) + 'V')
+
+mcp_voltage = chan.voltage
+mcp_value = chan.value
+
+# Convert to Temp
+temp = (mcp_voltage - mcp_0temp)/mcp_coefficient
