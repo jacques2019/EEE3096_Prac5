949707752a2567580d148991a88a9df234447c62
jacques2019 <jacques2019@gmail.com>
2020-10-29 14:53:44 +0200

    Output test1

--- ReadTemps.py
+++ ReadTemps.py
@@ -16,10 +16,11 @@ cs = digitalio.DigitalInOut(board.D5)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
+# create an analog input channel on pin 0 and 1
 chan_temp = AnalogIn(mcp, MCP.P1)
 chan_LDR = AnalogIn(mcp, MCP.P0)
 
+# Read values from ADC
 temp_voltage = chan_temp.voltage
 temp_value = chan_temp.value
 
@@ -28,7 +29,10 @@ LDR_voltage = chan_LDR.voltage
 
 # Convert to Temp
 temp = (temp_voltage - temp_0volt)/temp_coefficient
-resistance = 3.3 - LDR_voltage
 
-print(temp)
-print('{0:.3f}V over the LDR'.format(LDR_voltage))
+# Change resisitor voltage to LDR voltage
+LDR_reading = 3.3 - LDR_voltage
+
+print('Runtime\tTemp Reading\tTemp')
+print('{0}s\t\t{1}\t\t{2}\t\t'.format(1, temp_value, temp))
+print('{0:.3f}V over the LDR'.format(LDR_reading))
